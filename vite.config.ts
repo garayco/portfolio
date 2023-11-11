d0894c46ed44f9b3b4d14a6b51447e9451eb7c50
garayco <fgarayco@gmail.com>
2023-11-10 21:28:51 -0500

    /*

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "garayco.github.io/portfolio",
+  base: "garayco.github.io/portfolio/*",
   plugins: [
     preact({
       babel: {
